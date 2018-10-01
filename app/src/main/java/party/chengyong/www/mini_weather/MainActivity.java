41fe64bee2efab5c501ddd4c45346e1f71bfc83e
chengyong <willcy1006@163.com>
2018-10-01 16:40:05 +0800

    add navigation bar

---
+++ app/src/main/java/party/chengyong/www/mini_weather/MainActivity.java
@@ -0,0 +1,13 @@
+package party.chengyong.www.mini_weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
