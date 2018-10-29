36c7e2b171e7c5b69e3305bd465eeff22848263c
chengyong <willcy1006@163.com>
2018-10-29 10:09:24 +0800

    Added commands to SelectCity Activity

--- app/src/main/java/party/chengyong/www/mini_weather/SelectCity.java
+++ app/src/main/java/party/chengyong/www/mini_weather/SelectCity.java
@@ -33,6 +33,7 @@ public class SelectCity extends Activity implements View.OnClickListener {
     private ListAdapter listAdapter;
     private String cityCode = "101010100";
 
+//    當Activity準備要產生時，先呼叫onCreate方法
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,9 +43,12 @@ public class SelectCity extends Activity implements View.OnClickListener {
 
     }
 
+//    初始化介面
     private void initViews() {
         mBackBtn = (ImageView) findViewById(R.id.title_back);
         mBackBtn.setOnClickListener(this);
+
+//        實例化 MyApplication
         app = (MyApplication) getApplication();
         mCityList = app.getCityList();
         for (City city:mCityList){
@@ -52,23 +56,33 @@ public class SelectCity extends Activity implements View.OnClickListener {
         }
         listView = (ListView) findViewById(R.id.title_list);
 
+//        ArrayAdapter可將一組數組傳入ListView中去顯示出來
         ArrayAdapter adapter = new ArrayAdapter(this,
                 android.R.layout.simple_list_item_1,
                 mCityList);
         listView.setAdapter(adapter);
 
+//        ListView 內容的點擊事件
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                獲取該項item的內容（City類型）
                 City city = (City)parent.getItemAtPosition(position);
+//
+//              將點擊內容用Toast顯示出來
                 Toast toast = Toast.makeText(SelectCity.this,
                         "你選中的是: "+city.getCity()+" "+city.getNumber(), Toast.LENGTH_LONG);
                 toast.show();
 
+//                Intent(意圖)代表使用者與應用程式的互動，互動通常會產生變化，
+//                例如按下一個圖示後進行撥出電話，或者按下一個按鈕後轉換到另一個畫面(也就是另一個Activity)
                 Intent appInfo = new Intent(SelectCity.this, MainActivity.class);
 
+//                putExtra方法能傳遞簡單的值，將cityCode傳回給 MainActivity
                 appInfo.putExtra("cityCode",city.getNumber());
                 setResult(RESULT_OK, appInfo);
+
+                //退棧（返回前一個Activity）
                 finish();
                 //startActivity(appInfo);
 
